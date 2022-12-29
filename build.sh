f0ade87934eb6a8489ecd3b6b818cd2240c742a6
pavanbhat19999 <prbhat07@gmail.com>
2022-12-29 21:36:32 +0530

    update zola

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+echo "Pushing changes to zola-website"
+git add .
+git commit -m "update zola "
+git push
 zola build
 cp -r public/* ../pavanbhat1999.github.io/
 cd ../pavanbhat1999.github.io/
